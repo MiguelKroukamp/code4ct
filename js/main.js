d8ec30ec97a68d0faf60fa29c77c41d79c9a6ecb
MiguelKroukamp <kroukampm@gmail.com>
2019-01-28 16:28:36 +0200

    added icons on meet out team section

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-var timeLeftForCoffee=0;
-var timeLeftForGirls=280;
-var timeLeftForProjects=2680;
-var timeLeftForHours=8265;
+// var timeLeftForCoffee=0;
+// var timeLeftForGirls=280;
+// var timeLeftForProjects=2680;
+// var timeLeftForHours=8265;
 
-var cupsOfCoffee=document.getElementById("cupsOfCoffee");
-var girls=document.getElementById("girls");
-var projects=document.getElementById("projects");
-var hoursCoding=document.getElementById("hoursCoding");
+// var cupsOfCoffee=document.getElementById("cupsOfCoffee");
+// var girls=document.getElementById("girls");
+// var projects=document.getElementById("projects");
+// var hoursCoding=document.getElementById("hoursCoding");
 
-var timer=setInterval(function(){
-cupsOfCoffee.innerHTML=timeLeftForCoffee;
-girls.innerHTML=timeLeftForGirls;
-projects.innerHTML=timeLeftForProjects;
-hoursCoding.innerHTML=timeLeftForHours;
+// var timer=setInterval(function(){
+// cupsOfCoffee.innerHTML=timeLeftForCoffee;
+// girls.innerHTML=timeLeftForGirls;
+// projects.innerHTML=timeLeftForProjects;
+// hoursCoding.innerHTML=timeLeftForHours;
 
-if (timeLeftForCoffee===3000) {
-clearInterval(timer);
-cupsOfCoffee.innerHTML="3000+";
-girls.innerHTML="780+";
-projects.innerHTML="3120+";
-hoursCoding.innerHTML="8765+";
-}
-timeLeftForGirls++;
-timeLeftForCoffee+=10;
-timeLeftForProjects++;
-timeLeftForHours++;
-},
-1);
+// if (timeLeftForCoffee===3000) {
+// clearInterval(timer);
+// cupsOfCoffee.innerHTML="3000+";
+// girls.innerHTML="780+";
+// projects.innerHTML="3120+";
+// hoursCoding.innerHTML="8765+";
+// }
+// timeLeftForGirls++;
+// timeLeftForCoffee+=10;
+// timeLeftForProjects++;
+// timeLeftForHours++;
+// },
+// 1);
 
 var isActive = false;
 
